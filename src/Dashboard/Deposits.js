fbe44196bc6b4b1e00b6dcf7de14c05a2265fc49
19uday <16co149.uday@nitk.edu.in>
2020-01-22 09:10:56 +0530

    initial commit

---
+++ src/Dashboard/Deposits.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Title from './Title';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+const useStyles = makeStyles({
+  depositContext: {
+    flex: 1,
+  },
+});
+
+export default function Deposits() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <div className="profilePic">
+        <AccountCircleIcon />
+      </div>
+    </React.Fragment>
+  );
+}
